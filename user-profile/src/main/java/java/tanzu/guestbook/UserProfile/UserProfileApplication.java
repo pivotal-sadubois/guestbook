afdba28b4e50fafba668a968609a9c4aa03eae3a
Sacha Dubois <sdubois@vmware.com>
2023-03-31 16:40:10 +0200

    new

---
+++ user-profile/src/main/java/java/tanzu/guestbook/UserProfile/UserProfileApplication.java
@@ -0,0 +1,13 @@
+package java.tanzu.guestbook.UserProfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProfileApplication.class, args);
+	}
+
+}
