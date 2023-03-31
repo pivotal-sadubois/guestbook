e625a7daf8b7eed21811abf8419032ee19762fc9
Sacha Dubois <sdubois@vmware.com>
2023-03-31 16:26:54 +0200

    generated with Spring Inizializer

---
+++ user-profiles/src/test/java/java/tanzu/guestbook/userprofiles/UserProfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package java.tanzu.guestbook.userprofiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProfilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
